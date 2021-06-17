0bd16e88060ce4f43112201a5b68d7e7c99e6405
Dom Golac <domgolac@gmail.com>
2021-06-17 03:10:39 +0000

    Update admin dashboard with counts

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,9 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['USERNAME'], password: ENV['PASSWORD']
 
   def show
+    @counts = {
+      categories: Category.count,
+      products: Product.count
+    }
   end
 end
