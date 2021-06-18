be090ccd0e962013b76aa53d50f2f43b70c15462
Dom Golac <domgolac@gmail.com>
2021-06-18 01:30:08 +0000

    Update admin controllers to use user auth

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,6 +1,8 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['USERNAME'], password: ENV['PASSWORD']
 
+  before_filter :authorize
+
   def show
     @counts = {
       categories: Category.count,
